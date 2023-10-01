a320fafa8235243a2012c79b19e475a3b30b2513
Simone <ssimone.siepi@itis.pr.it>
2023-10-01 16:12:55 +0200

    seconda versione

--- server.js
+++ server.js
@@ -2,7 +2,43 @@ const http=require("http")
 const fs=require("fs")
 const path=require("path")
 function requestHandler(req,res){
+    let percorsoFile="."+req.url;
+    if (percorsoFile==="./") {
+        percorsoFile="./Home.html";
+    }
+    const extname=path.extname(percorsoFile);
+    console.log(extname);
+    let estensioneFile
+    switch (extname) {
+        case ".html":
+            estensioneFile="text/html";
+            break;
+        case ".png":
+            estensioneFile="image/png";
+            break;
+        case ".jpg":
+            estensioneFile="image/jpg";
+            break;
+        case ".css":
+            estensioneFile="text/css";
+            break;
+        case ".js":
+            estensioneFile="text/javascript";
+            break;
     
+        default:
+            break;
+    }
+    fs.readFile(percorsoFile,function(error,content){
+        if (error) {
+            res.writeHead(404);
+        }
+        else{
+            res.writeHead(200,{"Content-Type":estensioneFile});
+            res.write(content,'utf-8');
+        }
+        res.end();
+    });
 }
 
 const server=http.createServer(requestHandler);
